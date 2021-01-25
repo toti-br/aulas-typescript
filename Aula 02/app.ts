0577e043b52f0f22625eefbd4305a53af0a7fa56
EngelFlores <engel.tads@gmail.com>
2021-01-25 17:29:07 -0300

    Add aula 02

---
+++ Aula 02/app.ts
@@ -0,0 +1,8 @@
+const dog = 'Sebastian';
+
+console.log(dog);
+console.log('Olá')
+console.log('Segunda');
+console.log('Mudando arquivo');
+
+
---
+++ Aula 02/data.ts
@@ -0,0 +1,2 @@
+console.log('Mandando dados...');
+console.log('Testando');
